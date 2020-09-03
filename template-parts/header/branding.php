46ae532a38faaf1445b5d9a6e5dff0051b461789
Denisa Lalikova <denisa.lalikova@artunlimiteusa.com>
2020-09-02 20:32:53 -0500

    initial commit

---
+++ template-parts/header/branding.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template part for displaying the header branding
+ *
+ * @package wp_rig
+ */
+
+namespace WP_Rig\WP_Rig;
+?>
+
+<div class="site-branding">
+	<div class="branding-wrapper">
+		<a href="/"><img src="/wp-content/uploads/2020/09/A_ALOGO_Aplus_new.jpg" id="logo" alt="" ></a>	
+	</div><!-- .branding-wrapper -->
+</div><!-- .site-branding -->
