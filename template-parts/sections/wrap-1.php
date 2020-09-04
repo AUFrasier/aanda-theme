877d10971258a1fb5237176d7066b47577806680
Denisa Lalikova <denisa.lalikova@artunlimiteusa.com>
2020-09-03 20:44:00 -0500

    worked on the card section

--- template-parts/sections/wrap-1.php
+++ template-parts/sections/wrap-1.php
@@ -11,7 +11,88 @@ namespace WP_Rig\WP_Rig;
 
 <div class="wrap wrap-1">
     <div class="container-fluid">
-        <div id="bond">
-		</div>
+
+    <div class="row">
+        <div class="col-6">
+            <div class="card card-flip mb-4 mt-4">
+                <div class="card-front card-front-1 text-white">
+                    <div class="card-body">
+                        <h3 class="card-title">Roofing</h3>
+                    </div>
+                </div>
+                <div class="card-back card-back-1 text-white">
+                    <div class="card-body">
+                        <h3 class="card-title">Roofing</h3>
+                        <p class="card-text">
+                            At A&A Services Home Improvement, our <br>
+                            professionals are here to help you solve all your <br>
+                            roofing problems. Our technician can tell you if you <br>
+                            need a simple repair from a leak or if your shingles <br>
+                            are too damaged and you may need replacement.
+                        </p>
+                        <a href="#" class="btn btn-outline-secondary">Roofing</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-6">
+            <div class="card card-flip mb-4 mt-4">
+                <div class="card-front card-front-2 text-white">
+                    <div class="card-body">
+                        <h3 class="card-title">Siding</h3>
+                    </div>
+                </div>
+                <div class="card-back card-back-2 text-white">
+                    <div class="card-body">
+                        <h3 class="card-title">Sidng</h3>
+                        <p class="card-text">Suprise this one has more more more more content on the back!</p>
+                        <a href="#" class="btn btn-outline-secondary">Action</a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+
+    <div class="row">
+        <div class="col">
+            <div class="card card-flip">
+                <div class="card-front card-front-3 text-white">
+                    <div class="card-body">
+                        <h3 class="card-title">Windows</h3>
+                    </div>
+                </div>
+                <div class="card-back card-back-3 text-white">
+                    <div class="card-body">
+                        <h3 class="card-title">Windows</h3>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-outline-secondary">Action</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card card-flip">
+                <div class="card-front card-front-4 text-white">
+                    <div class="card-body">
+                        <h3 class="card-title">Doors</h3>
+                    </div>
+                </div>
+                <div class="card-back card-back-4 text-white">
+                    <div class="card-body">
+                        <h3 class="card-title">Doors</h3>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-outline-secondary">Action</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    </div>
+
+    <a href="/contact-us">
+		<div class="btn-action">
+			<button type="button" class="btn btn-lg btn-outline-primary btnSecHeader">Free Estimate</button>
+		</div>		
+    </a>
 </div><!-- .wrap-1 -->
