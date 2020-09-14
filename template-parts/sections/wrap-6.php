787c64ac5852b845cb2c98a2c11116dbedd0a816
Denisa Lalikova <denisa.lalikova@artunlimiteusa.com>
2020-09-13 21:44:17 -0500

    working on resizing the buy local sec

--- template-parts/sections/wrap-6.php
+++ template-parts/sections/wrap-6.php
@@ -13,18 +13,30 @@ namespace WP_Rig\WP_Rig;
     <div class="container-fluid">
         <div class="row">
             <h2>BUY LOCAL AND JOIN THE A & A FAMILY…</h2>
+            <p class="first">
+                Established in 1982, our goal was to make A & A Services, Inc. the kind of company
+            </p> 
             <p>
-                Established in 1982, our goal was to make A & A Services, Inc. the kind of company <br>
-                that we’d like to do business with as a homeowner. We made a promise to ourselves <br>
-                that we would always deliver top quality workmanship with exceptional customer <br>
-                service. We knew that we would have to go “above and beyond” for our customers. <br>
+                that we’d like to do business with as a homeowner. We made a promise to ourselves
             </p>
+            <p>
+                that we would always deliver top quality workmanship with exceptional customer
+            </p>
+            <p>
+            service. We knew that we would have to go “above and beyond” for our customers.
+            </p>         
             <p class="second">
-                As a result, we have had the privilege of helping thousands of our neighbors make their <br>
-                dream of a more beautiful, comfortable home come true. Our main goal is to build <br>
-                trusted relationships through high quality product installation of roofing, siding, <br>
-                windows, and doors. <strong>We guarantee that “We’ll Make It Right” on every project.</strong> <br>
+                As a result, we have had the privilege of helping thousands of our neighbors make their
             </p>
+            <p>
+                dream of a more beautiful, comfortable home come true. Our main goal is to build
+            </p>
+            <p>
+                trusted relationships through high quality product installation of roofing, siding,
+             </p> 
+             <p class="third">
+                windows, and doors. <strong>We guarantee that “We’ll Make It Right” on every project.</strong>
+             </p>   
             <img src="/wp-content/uploads/2020/09/A_A_family.png" alt="">
             <img src="/wp-content/uploads/2020/09/A_ALOGO_Aplus_new.png" alt="" class="secImg">
             <a href="/contact-us">
