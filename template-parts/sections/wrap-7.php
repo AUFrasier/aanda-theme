437cfceee05626a2514872faa6330d10168955b2
Denisa Lalikova <denisa.lalikova@artunlimiteusa.com>
2020-09-14 23:57:55 -0500

    map section done

--- template-parts/sections/wrap-7.php
+++ template-parts/sections/wrap-7.php
@@ -12,26 +12,30 @@ namespace WP_Rig\WP_Rig;
 <div class="wrap wrap-7">
     <div class="container-fluid">
         <div class="row">
-            <div class="map">
-                <a href="https://www.google.com/maps/place/115+North+St,+Salem,+MA+01970/@42.4971041,-70.8661916,11.38z/data=!4m5!3m4!1s0x89e31476d8508127:0x1fde0f63929665d5!8m2!3d42.5271215!4d-70.9021308" target="_blank">
-                    <img src="/wp-content/uploads/2020/09/A_A_map.png" alt="">
-                </a>              
+            <div class="col-12 col-xl-6">
+                <div class="map">
+                    <a href="https://www.google.com/maps/place/115+North+St,+Salem,+MA+01970/@42.4971041,-70.8661916,11.38z/data=!4m5!3m4!1s0x89e31476d8508127:0x1fde0f63929665d5!8m2!3d42.5271215!4d-70.9021308" target="_blank">
+                        <img src="/wp-content/uploads/2020/09/A_A_map.png" alt="">
+                    </a>              
+                </div>
             </div>
-            <div class="text">
-                <h3>
-                    NOW SERVING THE <br>
-                    GREATER NORTH SHORE <br>
-                    OF BOSTON IN ESSEX <br>
-                    AND PARTS OF <br>
-                    MIDDLESEX COUNTIES!
-                </h3>
+            <div class="col-12 col-xl-6">
+                <div class="text">
+                    <h3>
+                        NOW SERVING THE <br>
+                        GREATER NORTH SHORE <br>
+                        OF BOSTON IN ESSEX <br>
+                        AND PARTS OF <br>
+                        MIDDLESEX COUNTIES!
+                    </h3>
 
-                <a href="/contact">
-		            <div class="btn-action">
-			            <button type="button" class="btn btn-lg btn-outline-primary btnSecHeader">Free Estimate</button>
-		            </div>		
-                </a>
-            </div>
+                    <a href="/contact">
+		                <div class="btn-action">
+			                <button type="button" class="btn btn-lg btn-outline-primary btnSecHeader">Free Estimate</button>
+		                </div>		
+                    </a>
+                </div>
+            </div>        
         </div>
     
     </div>
