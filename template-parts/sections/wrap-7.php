fac2c0ba26a1eecc6125d7096bbbd5635dbd5fe5
Denisa Lalikova <denisa.lalikova@artunlimiteusa.com>
2020-09-16 13:46:13 -0500

    newest changes

--- template-parts/sections/wrap-7.php
+++ template-parts/sections/wrap-7.php
@@ -15,7 +15,8 @@ namespace WP_Rig\WP_Rig;
             <div class="col-12 col-xl-6">
                 <div class="map">
                     <a href="https://www.google.com/maps/place/115+North+St,+Salem,+MA+01970/@42.4971041,-70.8661916,11.38z/data=!4m5!3m4!1s0x89e31476d8508127:0x1fde0f63929665d5!8m2!3d42.5271215!4d-70.9021308" target="_blank">
-                        <img src="/wp-content/uploads/2020/09/A_A_map.png" alt="">
+                        <!--- <img src="/wp-content/uploads/2020/09/A_A_map.png" alt=""> --->
+                        <img src="/wp-content/uploads/2020/09/A_A_Services-maps.jpg" alt="">
                     </a>              
                 </div>
             </div>
